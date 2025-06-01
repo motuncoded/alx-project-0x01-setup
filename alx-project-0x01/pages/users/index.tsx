24d80e9b3a7867e5769a6fe3afcf993762741a5f
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-06-01 17:27:30 +0100

    task 0 done

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,92 @@
 import React from 'react'
 import Header from "@/components/layout/Header";
+import UserCard from '@/components/common/UserCard';
 
-const Users: React.FC = () => {
+type Address = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+};
+
+type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+};
+
+type UserProps = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address?: Address;
+  company?: Company;
+};
+
+interface UsersPageProps {
+  users: UserProps[];
+}
+
+const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
-    <div><Header/></div>
-  )
+    <div className="flex flex-col h-screen">
+             <Header />
+              <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">Users Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Add User
+          </button>
+        </div>
+          {users?.map(({ id, name, username, email, phone, website, address, company }: UserProps) => (
+            <UserCard
+              id={id}
+              name={name}
+              username={username}
+              email={email}
+              phone={phone}
+              website={website}
+              address={
+                address ?? {
+                  street: "",
+                  suite: "",
+                  city: "",
+                  zipcode: "",
+                  geo: { lat: "", lng: "" }
+                }
+              }
+                company={
+                    company ?? {
+                    name: "",
+                    catchPhrase: "",
+                    bs: ""
+                    }
+                }
+              key={id}
+            />
+          ))}
+             </main>     
+      </div>
+      
+    );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
 }
 
 export default Users

b8733431e703a4b19e0cbd931619f1aa7c1722ec
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-06-01 17:30:34 +0100

    task 3 done

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,81 +1,35 @@
 import React from 'react'
 import Header from "@/components/layout/Header";
-import UserCard from '@/components/common/UserCard';
+import { PostProps } from "@/interfaces";
+import PostCard from "@/components/common/PostCard";
 
-type Address = {
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: string;
-  geo: {
-    lat: string;
-    lng: string;
-  };
-};
-
-type Company = {
-  name: string;
-  catchPhrase: string;
-  bs: string;
-};
-
-type UserProps = {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  phone: string;
-  website: string;
-  address?: Address;
-  company?: Company;
-};
-
-interface UsersPageProps {
-  users: UserProps[];
+interface PostsPageProps {
+  posts: PostProps[];
 }
 
-const Users: React.FC<UsersPageProps> = ({ users }) => {
+const Users: React.FC<PostsPageProps> = ({ posts }) => {
   return (
     <div className="flex flex-col h-screen">
-             <Header />
-              <main className="p-4">
+      <Header />
+      <main className="p-4">
         <div className="flex justify-between">
           <h1 className=" text-2xl font-semibold">Users Content</h1>
           <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
             Add User
           </button>
         </div>
-          {users?.map(({ id, name, username, email, phone, website, address, company }: UserProps) => (
-            <UserCard
-              id={id}
-              name={name}
-              username={username}
-              email={email}
-              phone={phone}
-              website={website}
-              address={
-                address ?? {
-                  street: "",
-                  suite: "",
-                  city: "",
-                  zipcode: "",
-                  geo: { lat: "", lng: "" }
-                }
-              }
-                company={
-                    company ?? {
-                    name: "",
-                    catchPhrase: "",
-                    bs: ""
-                    }
-                }
-              key={id}
-            />
-          ))}
-             </main>     
-      </div>
-      
-    );
+        {posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+          <PostCard
+            title={title}
+            body={body}
+            userId={userId}
+            id={id}
+            key={key}
+          />
+        ))}
+      </main>
+    </div>
+  );
 }
 
 export async function getStaticProps() {
@@ -89,4 +43,4 @@ export async function getStaticProps() {
   };
 }
 
-export default Users
+export default Users;
